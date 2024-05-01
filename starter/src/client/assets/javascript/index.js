0fb9a867e5f639ed05976baddc48d0c9584c565a
Ivan Brunetto <ivanbrunetto@gmail.com>
2024-05-01 06:19:53 -0300

    implement rage progress by %

--- starter/src/client/assets/javascript/index.js
+++ starter/src/client/assets/javascript/index.js
@@ -2,6 +2,8 @@
 let store = {
 	track_id: undefined,
 	track_name: undefined,
+	track_size: undefined, 
+	track_size_by_segment: undefined,
 	player_id: undefined,
 	player_name: undefined,
 	race_id: undefined,
@@ -90,6 +92,7 @@ async function handleCreateRace() {
 	
 	console.log("RACE: ", race)
 	store.race_id = race.ID;
+	calculateTrackSize(race.Track);
 	
 	// The race has been created, now start the countdown
 	await runCountdown();
@@ -177,6 +180,22 @@ function handleSelectTrack(target) {
 function handleAccelerate() {
 	console.log("accelerate button clicked")
 	accelerate(store.race_id);
+}
+
+function calculateTrackSize(track) {
+	//initialize
+	store.track_size = 0;
+	store.track_size_by_segment = [];
+	
+	//calculate total size
+	track.segments.map(segment => store.track_size += segment);
+	
+	//calculate accumulated size for each segment
+	let count = 0;
+	track.segments.reduce((accumulatedSize, currentSize) => {
+		store.track_size_by_segment[count++] = accumulatedSize + currentSize;
+		return accumulatedSize + currentSize;
+	}, 0);
 }
 
 // HTML VIEWS ------------------------------------------------
@@ -290,11 +309,25 @@ function raceProgress(positions) {
 	positions = positions.sort((a, b) => (a.segment > b.segment) ? -1 : 1)
 	let count = 1
 
+	const progress = `
+		<tr>
+			<td>
+				<h2>${`Progress: ${
+					Math.round(
+						!positions[0].segment ? 0 : 
+						(store.track_size_by_segment[positions[0].segment - 1] / 
+						store.track_size) 
+						* 100)
+				}%`}
+			</td>
+		</tr>
+	`;
+	
 	const results = positions.map(p => {
 		return `
 			<tr>
 				<td>
-					<h3>${count++} - ${p.driver_name}</h3>
+					<h3>${count++} - ${p.driver_name} - ${'@segment: ' + p.segment}</h3>
 				</td>
 			</tr>
 		`
@@ -302,6 +335,7 @@ function raceProgress(positions) {
 
 	return `
 		<table>
+			${progress}
 			${results.join('')}
 		</table>
 	`
